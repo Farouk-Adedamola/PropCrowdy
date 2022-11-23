3228fe432466a1fa91fd4a59fc0070f37831b2e8
Farouk adedamola <faroukadedamola1@gmail.com>
2022-11-23 17:28:31 +0100

    commit changes

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,25 +1,38 @@
 import React, { Fragment } from "react";
 import classes from "./Header.module.css";
-import headerimage from "../../Assets/womanImage.png";
-import manradius from "../../Assets/man2.png";
+
+//  images import
+import group11 from "../../Assets/group22.png";
+import whatsapp from "../../Assets/WhatsAppicon.svg";
+// import manradius from "../../Assets/man2.png";
+
+// React icons
+
+import { FaPlay } from "react-icons/fa";
 
 const Header = () => {
   return (
     <Fragment>
       <main className={classes.mainArea}>
         <section className={classes.section}>
-          <img src={headerimage} alt="" />
-          <div className={classes.part}>
-            <img src={manradius} alt="" />
-            <div></div>
-          </div>
-          <div></div>
-          <div></div>
+          <img src={group11} alt="" />
         </section>
-        <section>
-          <div></div>
-          <div></div>
-          <div></div>
+        <section className={classes.second_Section}>
+          <h1>Real Estate Investment Oppotunities For Property Development</h1>
+          <p>
+            PropCrowdy is a crowdfunding investment platform that empowers
+            Nigerians to make smart <br /> investments in real estate
+          </p>
+          <div className={classes.buttons}>
+            <button>Get Started</button>
+            <div>
+              <FaPlay className={classes.icon} />
+              <p>See how it works</p>
+            </div>
+          </div>
+          <div className={classes.whatsapp}>
+            <img src={whatsapp} alt="whatsapp-icon" />
+          </div>
         </section>
       </main>
     </Fragment>
