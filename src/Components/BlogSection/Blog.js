0318b241a10e7ee700ba54bb3a060aa29aa0b90a
Farouk adedamola <faroukadedamola1@gmail.com>
2022-11-26 03:41:44 +0100

    commit new changes

--- src/Components/BlogSection/Blog.js
+++ src/Components/BlogSection/Blog.js
@@ -1,10 +1,10 @@
 import React from "react";
 import grouplist from "../../Assets/grouplist1.svg";
-
+import classes from "./Blog.module.css";
 const Blog = () => {
   return (
-    <div>
-      <h1>Articles From Our Blog</h1>
+    <div className={classes.blog}>
+      <h1 className={classes.head}>Articles From Our Blog</h1>
       <p>Stay updated with PropCrowdy trends and market updates</p>
       <img src={grouplist} alt="grouplist" />
     </div>
