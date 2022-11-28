4b0ae469e89c46b24c4f00d543fa2d27659bee8d
Farouk adedamola <faroukadedamola1@gmail.com>
2022-11-28 21:37:47 +0100

    commit react spinner

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
+import DotLoader from "react-spinners/ClipLoader";
 import Header from "./Components/Header/Header";
 import PeoplePlay from "./Components/PeoplePlay/PeoplePlay";
 import AsInvestor from "./Components/AsInvestor/AsInvestor";
@@ -12,20 +13,44 @@ import "./App.css";
 import Footer from "./Pagination/Footer/Footer";
 import AsDeveloper from "./Components/AsInvestor/AsDeveloper";
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
   return (
-    <Fragment>
-      <Header />
-      <PeoplePlay />
-      <AsInvestor />
-      <InvestorCards />
-      <AsDeveloper />
-      <FormSection />
-      <ChooseProp />
-      <Blog />
-      <FundedCompanies />
-      <Contact />
-      <Footer />
-    </Fragment>
+    <div>
+      {loading ? (
+        <DotLoader
+          color={"#FF0000"}
+          loading={loading}
+          size={40}
+          cssOverride={{
+            display: "block",
+            margin: "40vh auto",
+            // transform: "translateY(50vh)",
+          }}
+        />
+      ) : (
+        <div>
+          <Header />
+          <PeoplePlay />
+          <AsInvestor />
+          <InvestorCards />
+          <AsDeveloper />
+          <FormSection />
+          <ChooseProp />
+          <Blog />
+          <FundedCompanies />
+          <Contact />
+          <Footer />
+        </div>
+      )}
+    </div>
   );
 }
 
